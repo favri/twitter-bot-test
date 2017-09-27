688b7e6cc52f28a15c54aefde2607e794638f0a0
Favric <fabricio@leadaki.com>
2017-09-27 18:17:36 -0300

    Test

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ module.exports = {
   consumer_key: 'iYOYSN34gthKHHnZBw0h5amvE',
   consumer_secret: 'JEB7qVlZoBZCZ6fxp7pbsz7bzs9MUaP0Y6Lnwk8sNmztHvsf5s',
   access_token: '24933766-0RGbiwDMjPRRcoEBzNRgbyEH4OKeaA7rC5FzfLyzP',
-  access_token_secret: 'G7rOKgKAlkQGEi2z7lkufnF2vOqWfChtFYhKpE2O9sM2T'
+  access_token_secret: 'G7rOKgKAlkQGEi2z7lkufnF2vOqWfChtFYhKpE2O9sM2T',
 };
