9468f699c983fc55bd58867248c8b9f6baa55b84
Favric <fabricio@leadaki.com>
2017-09-27 10:11:40 -0300

    First Commit

---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by cliengo on 25/09/17.
+ */
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+  consumer_key: 'iYOYSN34gthKHHnZBw0h5amvE',
+  consumer_secret: 'JEB7qVlZoBZCZ6fxp7pbsz7bzs9MUaP0Y6Lnwk8sNmztHvsf5s',
+  access_token: '24933766-0RGbiwDMjPRRcoEBzNRgbyEH4OKeaA7rC5FzfLyzP',
+  access_token_secret: 'G7rOKgKAlkQGEi2z7lkufnF2vOqWfChtFYhKpE2O9sM2T'
+};
