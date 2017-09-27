6d26376f99fcc8e239bcde6d16cfc89aa1b70a54
Favric <fabricio@leadaki.com>
2017-09-27 10:37:43 -0300

    Added Engines

--- bot.js
+++ bot.js
@@ -4,7 +4,9 @@
 
 var twit = require ('twit');
 var config = require ('./config.js');
-var Twitter = new twit (config);
+var T = new twit (config);
+var cfg = require('./cf');
+
 var retweet = function() {
   var params = {
     q: '#chatbot, #Chatbot',
@@ -12,13 +14,13 @@ var retweet = function() {
     lang: 'es'
   }
 
-Twitter.get('search/tweets', params, function(err, data) {
+T.get('search/tweets', params, function(err, data) {
   // if there no errors
   if (!err) {
     // grab ID of tweet to retweet
     var retweetId = data.statuses[0].id_str;
     // Tell TWITTER to retweet
-    Twitter.post('statuses/retweet/:id', {
+    T.post('statuses/retweet/:id', {
       id: retweetId
     }, function(err, response) {
       if (response) {
@@ -41,3 +43,60 @@ Twitter.get('search/tweets', params, function(err, data) {
 retweet();
 // retweet in every 50 minutes
 setInterval(retweet, 3000000);
+
+// Putting together a context free grammar to make tweets
+var cfree = new cfg.ContextFree();
+cfree.addRule('S', ['TOPIC', 'THING', 'with', 'TECH']);
+
+// Thing can expand to join with another topic
+cfree.addRule('THING', ['TOPIC', 'THING', 'with', 'TECH']);
+
+// Tech can expand to be multiple technologies
+cfree.addRule('TECH', ['TECH', 'and', 'TECH']);
+cfree.addRule('TECH', ['TECH', 'and', 'TECH', 'plus', 'TECH']);
+
+// TOPICS
+cfree.addRule('TOPIC', ['Climate Change']);
+cfree.addRule('TOPIC', ['Poetry']);
+cfree.addRule('TOPIC', ['Music Box']);
+
+// THINGS
+cfree.addRule('THING', ['App']);
+cfree.addRule('THING', ['Sculpture']);
+cfree.addRule('THING', ['Wearable']);
+
+// TECHS
+cfree.addRule('TECH', ['JavaScript']);
+cfree.addRule('TECH', ['Arduino']);
+cfree.addRule('TECH', ['Oculus Rift']);
+cfree.addRule('TECH', ['Makerbot']);
+cfree.addRule('TECH', ['Cardboard']);
+cfree.addRule('TECH', ['Pencil']);
+cfree.addRule('TECH', ['Processing']);
+cfree.addRule('TECH', ['Python']);
+
+// Start once
+tweeter();
+
+// Once every N milliseconds
+setInterval(tweeter, 60*5*1000);
+
+// Here is the bot!
+function tweeter() {
+
+  // This is a random number bot
+  var tweet = cfree.getExpansion('S');
+
+  // Post that tweet!
+  T.post('statuses/update', { status: tweet }, tweeted);
+
+  // Callback for when the tweet is sent
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Success: ' + data.text);
+      //console.log(response);
+    }
+  };
+}
