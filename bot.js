007dd5c9bf926fd4d1fc6c5cba8f9769bef660a8
Favric <fabricio@leadaki.com>
2017-09-27 17:37:08 -0300

    Test

--- bot.js
+++ bot.js
@@ -1,75 +1,43 @@
+/**
+ * Created by cliengo on 25/09/17.
+ */
+
 var twit = require ('twit');
 var config = require ('./config.js');
-var T = new twit (config);
-
-var retweetChatbot = function() {
+var Twitter = new twit (config);
+var retweet = function() {
   var params = {
     q: 'Santiago Maldonado',
     result_type: 'recent',
     lang: 'es'
   }
 
-T.get('search/tweets', params, function(err, data) {
+Twitter.get('search/tweets', params, function(err, data) {
   // if there no errors
   if (!err) {
     // grab ID of tweet to retweet
     var retweetId = data.statuses[0].id_str;
     // Tell TWITTER to retweet
-    T.post('statuses/retweet/:id', {
+    Twitter.post('statuses/retweet/:id', {
       id: retweetId
     }, function(err, response) {
       if (response) {
-        console.log('Retweeted Chatbot!!!');
+        console.log('Retweeted!!!');
       }
       // if there was an error while tweeting
       if (err) {
-        console.log('Something went wrong while RETWEETING Chatbot... Duplication maybe...');
+        console.log('Something went wrong while RETWEETING... Duplication maybe...');
       }
     });
   }
   // if unable to Search a tweet
   else {
-    console.log('Something went wrong while SEARCHING Chatbot...');
-  }
-});
-// };
-var retweetNodeJs = function() {
-  var params = {
-    q: 'Santiago Maldonado',
-    result_type: 'popular',
-    lang: 'en'
-  }
-
-T.get('search/tweets', params, function(err, data) {
-  // if there no errors
-  if (!err) {
-    // grab ID of tweet to retweet
-    var retweetId = data.statuses[0].id_str;
-    // Tell TWITTER to retweet
-    T.post('statuses/retweet/:id', {
-      id: retweetId
-    }, function(err, response) {
-      if (response) {
-        console.log('Retweeted Node!!!');
-      }
-      // if there was an error while tweeting
-      if (err) {
-        console.log('Something went wrong while RETWEETING Node... Duplication maybe...');
-      }
-    });
-  }
-  // if unable to Search a tweet
-  else {
-    console.log('Something went wrong while SEARCHING Node...');
+    console.log('Something went wrong while SEARCHING...');
   }
 });
 };
 
 // grab & retweet as soon as program is running...
-retweetChatbot();
-retweetNodeJs();
-
+retweet();
 // retweet in every 50 minutes
-setInterval(retweetChatbot, 21400000);
-// retweet in every 50 minutes
-setInterval(retweetNodeJs, 18000000);
+setInterval(retweet, 3000000);
