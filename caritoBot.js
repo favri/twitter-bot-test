3a5dc42f2b5a191a68a7778c21cab4468039dc52
Favric <fabricio@leadaki.com>
2017-10-09 14:13:47 -0300

    Added mongodb

--- caritoBot.js
+++ caritoBot.js
@@ -1,48 +1,60 @@
 const twit = require ('twit');
 const config = require ('./config');
 const T = new twit (config);
+const mongodb = require('mongodb');
 
-let caritosId = '182386151';
-// let stream = T.stream('statuses/filter', { follow: caritosId });
-let answeredTweets = ['916308363922825216','916312666779586560','916309650580103168'];
-;
-const caritosTweets = function() {
-  let params = {
-    q: 'from:@Carito_granate to:@ElBaileDeOdin since:2017-10-05'
-  };
+const uri = 'mongodb://favri:fabric10@ds115045.mlab.com:15045/heroku_xzvnnj4p';
+// const caritosId = '182386151';
+const tweetsArray = [];
+const getIds = function (){
+  mongodb.MongoClient.connect(uri, function(err, db) {
+    if(err) throw err;
 
+    const answeredTweets = db.collection('answeredTweets');
+    answeredTweets.find().toArray(function (err, docs) {
+      if(err) throw err;
+      docs.forEach(function (doc) {
+        tweetsArray.push(doc._id);
+      });
+    });
 
-  T.get('search/tweets', params, function(err, data) {
-    if (!err) {
-      // grab ID of tweet to reply
-      data.statuses.forEach(s => {
+    let params = {
+      q: 'from:@Carito_granate to:@ElBaileDeOdin since:2017-10-05'
+    };
 
-        if (answeredTweets.indexOf(s.id_str) > -1) {
-          console.log('Already Answered');
-        }
-        else {
-          console.log('not answered ' + s.id_str);
+    T.get('search/tweets', params, function(err, data) {
+      if (!err) {
 
-          T.post('statuses/update', {
-            status: '@' + s.user.screen_name + ' Besito chau. El Bot',
-            in_reply_to_status_id: s.id_str
-          }, (err, data, response) => {
-            if (err) {
-              console.log(err)
-            } else {
-              answeredTweets.push(s.id_str);
-              console.log(`${data.text} tweeted!`)
-            }
-          })
-        }
-      })
+        // grab ID of tweet to reply
+        data.statuses.forEach(s => {
 
-    }
-    else {
-      console.log('Something went wrong while SEARCHING...');
-    }
+          if (tweetsArray.indexOf(s.id_str) > -1) {
+            console.log('Already Answered');
+          }
+          else {
+            console.log('not answered');
+
+            T.post('statuses/update', {
+              status: '@' + s.user.screen_name + ' Besito chau. El Bot',
+              in_reply_to_status_id: s.id_str
+            }, (err, data, response) => {
+              if (err) {
+                console.log(err)
+              } else {
+                answeredTweets.insertOne({"_id": s.id_str});
+                console.log(`${data.text} tweeted!`)
+              }
+            })
+          }
+        })
+
+      }
+      else {
+        console.log('Something went wrong while SEARCHING...');
+      }
+    });
   });
 };
 
-caritosTweets();
-setInterval(caritosTweets,50000);
+getIds();
+setInterval(getIds,50000);
